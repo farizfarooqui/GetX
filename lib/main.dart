9a21f1212755a7af2e93d8836f98efa66ae91578
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-07-18 17:15:00 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/Views/Counter%20view/counter_view.dart';
-import 'package:getx/Views/Favourite%20view/favourite_view.dart';
-import 'package:getx/Views/Image%20picker%20view/image_picker_view.dart';
-import 'package:getx/Views/Slider%20view/slider_view.dart';
-import 'package:getx/Views/Switch%20view/switch_button_view.dart';
 
 void main() {
   runApp(const MyApp());
